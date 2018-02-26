3f8e24ff77b14c1614be2ca1818034ce1081b0ee
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-26 14:00:00 +0530

    Update RL.C

--- RL.C
+++ RL.C
@@ -1,15 +1,13 @@
 #include<stdio.h>
- 
+ #include<conio.h>
 int main()
 {
     int a[50],i,n,large,small;
     printf("How many elements:");
     scanf("%d",&n);
     printf("Enter the Array:");
- 
     for(i=0;i<n;++i)
         scanf("%d",&a[i]);
-    
     large=small=a[0];
     for(i=1;i<n;++i)
     {
@@ -18,9 +16,7 @@ int main()
         if(a[i]<small)
             small=a[i];
     }
-    
     printf("The largest element is %d",large);
     printf("\nThe smallest element is %d",small);
- 
-    return 0;
+  return 0;
 }
