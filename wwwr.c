16253edef9f72a19dd362486444d130e43d2fde6
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-26 13:45:56 +0530

    Update wwwr.c

--- wwwr.c
+++ wwwr.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
+#include<conio.h>
 int main()
 {
     long long n;
     int count = 0;
-
     printf("Enter an integer: ");
     scanf("%lld", &n);
-
     while(n != 0)
     {
-        // n = n/10
         n /= 10;
         ++count;
     }
