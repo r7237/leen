38b2f14a1b638a37f389719efe8321484ea8c469
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-26 14:25:26 +0530

    Update FGD.C

--- FGD.C
+++ FGD.C
@@ -1,16 +1,13 @@
 #include <stdio.h>
- 
+ #include<conio.h>
 int main()
 {
     int num;
     int tempNum,flag;
-     
     printf("Enter an integer number: ");
     scanf("%d",&num);
-     
     tempNum=num;
     flag=0;
-    /*check power of two*/
     while(tempNum!=1)
     {
         if(tempNum%2!=0){
@@ -19,11 +16,9 @@ int main()
         }
         tempNum=tempNum/2;
     }
-  
     if(flag==0)
         printf("%d is a number that is the power of 2.",num);
     else
-        printf("%d is not the power of 2.",num);
-      
+        printf("%d is not the power of 2.",num); 
     return 0;
 }
