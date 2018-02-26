bebe02942d51a51fcc8b78805eb9eae6a1b24c3e
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-26 13:30:03 +0530

    Update Leen.c

--- Leen.c
+++ Leen.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-
+#include,conio.h>
 int main()
 {
     char a[100], b[100];
-    
     printf("Enter the first string\n");
     gets(a);
-    
     printf("Enter the second string\n");
     gets(b);
-    
     strcat(a,b);
-    
     printf("String obtained on concatenation is %s\n",a);
-    
     return 0;
 
 }
