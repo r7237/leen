1bb10df940d9b1110a3fd147712b119c225b24fc
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-26 13:18:45 +0530

    Update README.m.c

--- README.m.c
+++ README.m.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 #include <conio.h>
 void main(){
      char name[20];
-     int i,j,k;
+     int i,j;
      clrscr();
      printf("\nEnter a string : ");
      scanf("%s",name);
@@ -11,8 +10,6 @@ void main(){
     printf("\n");
     for(j=i+1;j<strlen(name);j++)
           printf("%c",name[j]);
-    for(k=0;k<=i;k++)
-          printf("%c",name[k]);
      }
 getch();
 }
